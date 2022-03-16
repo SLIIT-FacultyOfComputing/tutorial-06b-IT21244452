f87f1f65ddaffe7577e99b83a29c25751dc39736
IT21244452 <99301219+IT21244452@users.noreply.github.com>
2022-03-16 19:15:55 +0000

    I completed Student.cpp file

--- Student.cpp
+++ Student.cpp
@@ -4,13 +4,15 @@
 using namespace std;
 
 // Assign studentId and name
-void Student::assignDetails(int no, char name[]) {
+void Student::assignDetails(int no, char name[])
+{
   studentID = no;
   strcpy(studentName , name);
 }
 
 // Display StudentId and Name
-void Student::display() {
+void Student::display()
+{
   cout << "Student ID : " << studentID << endl;
   
   cout << "Student name : " << studentName << endl;
